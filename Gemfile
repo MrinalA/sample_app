4f5c12cc4763827aa52e2371f0c5bdd069c6cdd5
Mrinal Agarwal <mrinal@infotechoutreach.org>
2012-12-05 15:47:19 -0800

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -33,7 +33,9 @@ gem 'jquery-rails' , '2.0.2'
 
 group :test do
 	gem 'capybara', '2.0.1'
+	gem 'factory_girl_rails', '4.1.0'
 end
+
 
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,15 @@ describe User do
                     password: "foobar", password_confirmation: "foobar") 
 end
 
+describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
 
   subject { @user }
 
